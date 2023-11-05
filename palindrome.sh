545a19c7a53cc90d0a2c114a7941c4c755cf04b3
Sravanthi chakravaram <sravanthich1305@gmail.com>
2023-11-05 18:24:42 -0500

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	loops.sh

---
+++ palindrome.sh
@@ -0,0 +1,21 @@
+###Palindrome number
+echo "Enter a number: "
+read number
+
+reverse=0
+original=$number
+
+while [ $number -ne 0 ]
+do
+  reminder=$(( $number % 10 ))
+  reverse=$(( $reverse * 10 + $remainder ))
+  number=$(( $number /10 ))
+done
+
+if [ $original -eq $reverse ]
+then
+  echo "$original is a Palindrome."
+else
+  echo "$original is not a palindrome."
+
+fi
