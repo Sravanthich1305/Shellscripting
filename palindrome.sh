a838edd68cc7168ea3acecb9bc59180fb1a83b32
Sravanthi chakravaram <sravanthich1305@gmail.com>
2023-11-05 18:45:39 -0500

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	loops.sh

--- palindrome.sh
+++ palindrome.sh
@@ -8,12 +8,12 @@ original=$number
 while [ $number -ne 0 ]
 do
 
-    remainder=$(( $number % 10 ))
-    reverse=$(( $reverse * 10 + $remainder ))
-    number=$(( $number / 10 ))
-  ##reminder=$(( $number % 10 ))
-  ##reverse=$(( $reverse * 10 + $remainder ))
-  ##number=$(( $number / 10 ))
+    ##remainder=$(( $number % 10 ))
+    ##reverse=$(( $reverse * 10 + $remainder ))
+    ##number=$(( $number / 10 ))
+  remainder=$(( $number % 10 ))
+  reverse=$(( $reverse * 10 + $remainder ))
+  number=$(( $number / 10 ))
 done
 
 if [ $original -eq $reverse ]
