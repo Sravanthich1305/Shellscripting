71e6a1c93007abb249340625b9e9060df6ddc7fe
Sravanthi chakravaram <sravanthich1305@gmail.com>
2023-12-14 13:26:22 -0500

    adding values

---
+++ 01-Array.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+##exampe of array
+FRUITS=("apple" "banana" "chiku")
+
+echo "enter first fruit name ${FRUIT[0]}"
+echo "enter first fruit name ${FRUIT[1]}"
+echo "enter first fruit name ${FRUIT[2]}"
