3e48893dcf9b9fec847c6c8c9fe25badad1b5e47
Sravanthi chakravaram <sravanthich1305@gmail.com>
2023-12-14 13:30:31 -0500

    adding values

--- 01-Array.sh
+++ 01-Array.sh
@@ -5,4 +5,4 @@ FRUITS=("apple" "banana" "chiku")
 echo "enter first fruit name ${FRUITS[0]}"
 echo "enter second fruit name ${FRUITS[1]}"
 echo "enter third fruit name ${FRUITS[2]}"
-echo "ALL fruits name ${FRUTIS[@]}"
+echo "ALL fruits name ${FRUITS[@]}"
