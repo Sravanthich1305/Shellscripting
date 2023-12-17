ce674198828b538b9937bea853e11c4087e55f65
Sravanthi chakravaram <sravanthich1305@gmail.com>
2023-12-17 09:51:34 -0500

    adding values ..

--- couting_words.sh
+++ couting_words.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-WORD=$1
+WORD=DEVOPS
 echo "Enter a word $WORD"
 #couting a words using wc -l command
-echo "$WORD" | wc -w
-echo "Total word count is:: {$WORD[@]}"
+echo "$WORD" | wc -l
+echo "Total word count is:: ${WORD[@]}"
